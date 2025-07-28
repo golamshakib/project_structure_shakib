0e1e49daea0d61e6e532b3ca1c4915033e464a57
golamshakib <golamshakib.h@gmail.com>
2025-07-28 20:21:00 +0600

    added the .env in the project

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'app.dart';
 import 'core/services/Auth_service.dart';
 import 'core/utils/logging/loggerformain.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: '.env');
   await AuthService.init();
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]).then(
         (value) {
