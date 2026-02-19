2916e7f2039e82139d1f7d7bafa1a9b37687a20e
golamshakib <golamshakib.h@gmail.com>
2026-02-19 11:24:54 +0600

    modified the Custom dropdown class

--- lib/core/common/widgets/custom_dropdown.dart
+++ lib/core/common/widgets/custom_dropdown.dart
@@ -5,26 +5,37 @@ import 'custom_text.dart';
 import '../../utils/constants/app_colors.dart';
 
 class CustomDropdownField extends StatelessWidget {
+  final GlobalKey _menuKey = GlobalKey();
+
+  CustomDropdownField({
+    super.key,
+    this.label,
+    this.radius,
+    required this.hintText,
+    this.withAsterisk = false,
+    required this.items,
+    required this.selectedValue,
+    this.selectedItemColor,
+    this.borderColor = const Color(0xffB8B8B8),
+    required this.onChanged,
+    this.selectedBackgroundColor = AppColors.primary,
+  });
+
   final String? label;
   final String hintText;
   final bool withAsterisk;
   final List<String> items;
   final String selectedValue;
   final Color? borderColor;
+  final double? radius;
   final ValueChanged<String> onChanged;
-  final double? borderRedius;
+  final Color? selectedItemColor;
+  final Color? selectedBackgroundColor;
 
-  const CustomDropdownField({
-    super.key,
-    this.label,
-    required this.hintText,
-    this.withAsterisk = false,
-    required this.items,
-    required this.selectedValue,
-    this.borderColor = const Color(0xffB8B8B8),
-    required this.onChanged,
-    this.borderRedius,
-  });
+  void _openMenu() {
+    final dynamic state = _menuKey.currentState;
+    state.showButtonMenu();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -56,65 +67,81 @@ class CustomDropdownField extends StatelessWidget {
         //SizedBox(height: getHeight(6)),
 
         /// Dropdown field with PopupMenu
-        Container(
-          // height: getHeight(48),
-          padding: EdgeInsets.symmetric(
-            horizontal: getWidth(18),
-            vertical: getHeight(18),
-          ),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(borderRedius ?? 4),
-            border: Border.all(
-              color: AppColors.textFormFieldBorder,
-              width: getWidth(1),
+        GestureDetector(
+          onTap: _openMenu,
+          child: Container(
+            // height: getHeight(48),
+            padding: EdgeInsets.symmetric(
+              horizontal: getWidth(18),
+              vertical: getHeight(15),
             ),
-            color: AppColors.textWhite,
-          ),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              // Selected Value or Hint Text
-              CustomText(
-                textOverflow: TextOverflow.ellipsis,
-                fontWeight: FontWeight.w500,
-                fontSize: getWidth(14),
-                text: selectedValue.isEmpty ? hintText : selectedValue,
-                textColor: selectedValue.isEmpty
-                    ? AppColors.textPrimary
-                    : AppColors.textPrimary,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(radius ?? getWidth(32)),
+              border: Border.all(
+                color: AppColors.textFormFieldBorder,
+                width: getWidth(1),
               ),
+              color: AppColors.textWhite, // TODO: Need to Check
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                // Selected Value or Hint Text
+                CustomText(
+                  text: selectedValue.isEmpty ? hintText : selectedValue,
+                  fontSize: getWidth(14),
+                  fontWeight: FontWeight.w400,
+                  textColor:
+                  selectedValue.isEmpty
+                      ? AppColors.textPrimary
+                      : AppColors.textPrimary,
+                  textOverflow: TextOverflow.ellipsis,
+                ),
 
-              // Dropdown Icon with PopupMenuButton
-              PopupMenuButton<String>(
-                color: Colors.white,
-                onSelected: onChanged,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(8),
-                ),
-                constraints: BoxConstraints(
-                  maxWidth: getWidth(500),
-                  maxHeight: getHeight(400),
-                ),
-                offset: Offset(getWidth(0), getHeight(20)),
-                child: Icon(Icons.keyboard_arrow_down, size: getHeight(24)),
-                // Popup menu items
-                itemBuilder: (context) {
-                  return items.map((item) {
-                    return PopupMenuItem<String>(
-                      value: item,
-                      child: Align(
-                        alignment: Alignment.centerLeft,
-                        child: CustomText(
-                          text: item,
-                          fontWeight: FontWeight.w500,
-                          fontSize: getWidth(16),
+                // Dropdown Icon with PopupMenuButton
+                PopupMenuButton<String>(
+                  key: _menuKey,
+                  color: Colors.white,
+                  onSelected: onChanged,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  constraints: BoxConstraints(
+                    maxWidth: getWidth(500),
+                    maxHeight: getHeight(400),
+                  ),
+                  offset: Offset(getWidth(0), getHeight(20)),
+                  child: Icon(
+                    Icons.keyboard_arrow_down,
+                    size: getHeight(24),
+                    color: AppColors.primary,
+                  ),
+                  // Popup menu items
+                  itemBuilder: (context) {
+                    return items.map((item) {
+                      return PopupMenuItem<String>(
+                        value: item,
+                        child: Align(
+                          alignment: Alignment.centerLeft,
+                          child: CustomText(
+                            text: item,
+                            fontSize: getWidth(14),
+                            fontWeight:
+                            selectedValue == item
+                                ? FontWeight.w500
+                                : FontWeight.w400,
+                            textColor:
+                            selectedValue == item
+                                ? AppColors.primary
+                                : AppColors.textSecondary,
+                          ),
                         ),
-                      ),
-                    );
-                  }).toList();
-                },
-              ),
-            ],
+                      );
+                    }).toList();
+                  },
+                ),
+              ],
+            ),
           ),
         ),
       ],
